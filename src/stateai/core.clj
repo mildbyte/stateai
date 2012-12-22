684f291db002c3873d46eaa8c4440b8a5deac67f
Artjoms Iskovs <mildbyte@gmail.com>
2012-12-22 16:26:42 +0200

    Added a world and means to simulate it

--- src/stateai/core.clj
+++ src/stateai/core.clj
@@ -1,88 +1,81 @@
 (ns stateai.core)
 
 (def all-states
-  '(:wander :feed :sleep))
+  '(:wander :feed :sleep :mate))
 (def all-attributes
-  '(:hunger :health :fatigue))
+  '(:hunger :health :fatigue :age :children))
+(def additional-inputs
+  '(:nearest-agent-health))
+(def all-inputs (concat all-attributes additional-inputs))
 (def initial-attributes
   {:hunger 100
    :health 100
-   :fatigue 100})
+   :fatigue 100
+   :age 0
+   :children 0})
 
 (defrecord Agent [state attributes state-coeffs])
+(defrecord World [age agents])
+
+(defn square [x] (* x x))
+
+(defn distance [[x1 y1] [x2 y2]]
+  (+ (square (- x1 x2))
+     (square (- y1 y2))))
 
 (defn make-random-agent []
   "Creates a random wandering agent"
   (Agent. :wander
           initial-attributes
-          (into {} (for [st all-states, nst all-states, attr all-attributes]
+          (into {} (for [st all-states, nst all-states, attr all-inputs]
                      [[st nst attr] (dec (* (rand) 2))]))))
 
-(defn get-state-priority [agent new-state]
+(defn make-random-world [number-of-agents]
+  (World. 0
+          (into #{} (map (fn [agent] [[(- (* 2000 (rand)) 1000)
+                                      (- (* 2000 (rand)) 1000)]
+                                     agent])
+                         (repeatedly number-of-agents make-random-agent)))))
+
+(defn get-state-priority [agent new-state inputs]
   "Calculates the priority of an agent transferring into a new state"
   (apply +
          (map (fn [[attrname attrval]]
                 (* attrval
                    ((:state-coeffs agent) [(:state agent) new-state attrname])))
-              (:attributes agent))))
+              (conj (:attributes agent) inputs))))
 
 (defn state-is-possible [agent new-state]
   "The agent can't sleep if its fatigue is already at maximum etc."
   (case new-state
     :wander true
     :feed   (< (:hunger (:attributes agent)) 100)
-    :sleep  (< (:fatigue (:attributes agent)) 100)))
+    :sleep  (< (:fatigue (:attributes agent)) 100)
+    :mate   (< (:children (:attributes agent)) 1)))
 
-(defn get-next-state [agent]
+(defn get-next-state [agent inputs]
   "Gets the state with the highest priority for a given agent"
   (apply (partial max-key (fn [new-state]
-                             (get-state-priority agent new-state)))
+                             (get-state-priority agent new-state inputs)))
          (filter #(state-is-possible agent %) all-states)))
 
-(defn advance-agent [agent]
+(defn advance-agent [agent inputs]
   "Moves the agent to his next state and changes its attributes accordingly"
-  (Agent. (get-next-state agent)
-          {:hunger (if (= :feed (:state agent))
-                     (+ (:hunger (:attributes agent)) 5)
-                     (- (:hunger (:attributes agent)) 1))
+  (Agent. (get-next-state agent inputs)
+          {:hunger (- (:hunger (:attributes agent)) 1)
            :fatigue (if (= :sleep (:state agent))
                       (+ (:fatigue (:attributes agent)) 5)
-                      (- (:hunger (:attributes agent)) 1))
+                      (- (:fatigue (:attributes agent)) 1))
            :health (+ (:health (:attributes agent))
                       (if (<= (:hunger (:attributes agent)) 0) -1 0)
-                      (if (<= (:fatigue (:attributes agent)) 0) -1 0))}
+                      (if (<= (:fatigue (:attributes agent)) 0) -1 0)
+                      (if (> (:age (:attributes agent)) 500) -1 0))
+           :age (inc (:age (:attributes agent)))}
           (:state-coeffs agent)))
-
-(defn survives-time [agent max-time]
-  "Simulates an agent until max-time to see how long it survives"
-  (first (drop-while (fn [[survived currag]]
-                       (and (> (:health (:attributes currag)) 0)
-                            (< survived max-time)))
-                     (iterate (fn [[survived currag]]
-                                [(inc survived)
-                                 (advance-agent currag)])
-                              [0 agent]))))
-
-;100 random agents
-(def initial-population (take 100 (repeatedly make-random-agent)))
-
-;Calculates a list of times a given population survives
-(defn survivor-times [agents] (map #(first (survives-time % 20000)) agents))
-
-;A population that neither survives forever nor dies immediately
-;(doesn't eat or sleep; 100 ticks for the fatigue and hunger to go
-;down and then 50 ticks for them to bring the health down to 0)
-(def imperfect-population
-  (take 100
-        (filter (fn [agent] (let [[time ag] (survives-time agent 2000)]
-                             (and (< time 2000)
-                                  (> time 150))))
-                (repeatedly make-random-agent))))
-
 
 (defn combine-agent-coeffs [coeff1 coeff2]
   "Combines two sets of coefficients into one by averaging them"
-  (into {} (for [st all-states, nst all-states, attr all-attributes]
+  (into {} (for [st all-states, nst all-states, attr all-inputs]
              [[st nst attr] (/ (+ (coeff1 [st nst attr])
                                   (coeff2 [st nst attr]))
                                2)])))
@@ -94,3 +87,40 @@
           (combine-agent-coeffs (:state-coeffs agent1)
                                 (:state-coeffs agent2))))
 
+(defn is-alive [agent] (> (:health (:attributes agent)) 0))
+
+(defn advance-world [world]
+  (World.
+   (inc (:age world))
+   (into
+    #{}
+    (filter
+     (fn [[[x y] agent]] (is-alive agent))
+     (map
+      (fn [[[x y] agent]] [[x y] (advance-agent agent '())])
+      (reduce
+       (fn [processed-agents [[xcurr ycurr] current-agent]]
+         (let [by-distance
+               (sort-by
+                (fn [[[x y] agent]]
+                  (distance [xcurr ycurr]
+                            [x y])) processed-agents)]
+           (case (:state current-agent)
+             :wander
+             (conj processed-agents [[xcurr ycurr] current-agent])
+             :sleep
+             (conj processed-agents [[xcurr ycurr] current-agent])
+             :feed
+             (let [[[x y] victim] (second by-distance)]
+               (conj (if (= (:state victim) :sleep)
+                       processed-agents
+                       (disj processed-agents [[x y] victim]))
+                     [[xcurr ycurr] current-agent]))
+             :mate
+             (let [[[x y] partner] (second (filter (fn [[[x y] partner]]
+                                                     (= (:state partner) :mate))))]
+               (conj (if partner (conj processed-agents
+                                       [[0 0] (mate-agents current-agent partner)])
+                         processed-agents))))))
+       #{}
+       (:agents world)))))))
